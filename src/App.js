f839da8a7a6993b60eaed318a4acbe0778eb56d1
solfixafrica <solfixafrica@gmail.com>
2022-05-10 14:51:39 +0300

    Chore: set project

--- src/App.js
+++ src/App.js
@@ -1,65 +1,23 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-// import type { RouteObject } from "react-router-dom";
-// import { Outlet, Link, useRoutes, useParams, Router } from "react-router-dom";
-import Tapp from "./Pages/App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Tapp from "./Pages/App";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Error from "./Pages/Error";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-// const Routings = () => {
-//   let routes = useRoutes([
-//     { path: "/", element: <Home /> },
-//     { path: "about", element: <About /> },
-//     // ...
-//   ]);
-//   return routes;
-// };
-
 function App() {
-  // const routes = [
-  //   {
-  //     path: "/",
-  //     element: <Tapp />,
-  //     children: [
-  //       { index: true, element: <Home /> },
-  //       { path: "/about", element: <About /> },
-  //       // {
-  //       //   path: "/courses",
-  //       //   element: <Courses />,
-  //       //   children: [
-  //       //     { index: true, element: <CoursesIndex /> },
-  //       //     { path: "/courses/:id", element: <Course /> },
-  //       //   ],
-  //       // },
-  //       { path: "*", element: <Error /> },
-  //     ],
-  //   },
-  // ];
-  // let element = useRoutes(routes);
-
   return (
-    // <div>
-    // <Router>
-    //   <Routings />
-    // </Router>
-    // </div>
-
     <BrowserRouter>
-  <Routes>
-      <Route index element={<Home />} />
-  <Route path="/test" element={<Tapp />} />
-    <Route path="/about" element={<About />}/>
-    <Route path="*" element={<Error/>}/>
-
-  </Routes>
-</BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/test" element={<Tapp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
